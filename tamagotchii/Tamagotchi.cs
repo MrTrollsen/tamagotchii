8ebfcde76377b9f7a25a98860ce754ad686f6456
Loke <lowenbergpetterssonn.loke@elev.ga.ntig.se>
2022-09-01 11:04:42 +0200

    lskrjfhpw9ehtgpOE8FY

---
+++ tamagotchii/Tamagotchi.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace tamagotchii;
+
+public class Tamagotchi
+{
+    private int hunger;
+    private int boredom;
+    private List<string> words = new List<string>() { "hello", "feed me" };
+    private bool isAlive;
+
+    private Random rnd = new Random();
+
+    public string name;
+
+    public void feed();
+    {
+
+    }
+    public void hi();
+    {
+
+    }
+}
+
